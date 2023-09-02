1c4da698066bad05acbfb64d7a0d601b86da4c37
Derrick Hammer <derrick@derrickhammer.com>
2023-09-02 13:08:24 -0400

    dep: update module hashes

--- src/registry.ts
+++ src/registry.ts
@@ -2,16 +2,16 @@ const DEFAULT_MODULE_REGISTRY = new Map<string, string>(
   Object.entries({
     // swarm
     z3o47ar8NBrnaEneBVzZD7QuMRMXjDtQDCpt4xP6mhsdw1cjnJ8mQKfNKGv3:
-      "zduKEY6Z4xJ4mraWy3hCbx1TmBxueCtHCFdnjjoiX7fGJdrZAcJHTyJmbu",
+      "zduTR1y921Erd52dYq1DKLpA9J6sw8g8KPfUdbkqmwdXDGKCjQKL4SHvDd",
     // peerDiscoveryRegistry
     z3o47aaLSspwrXzmu5mjuHPwaq3gRbyYQ3jL9RM1ammuHGB7uxSFBK2dRjqR:
-      "z3o47aaLSspwrXzmu5mjuHPwaq3gRbyYQ3jL9RM1ammuHGB7uxSFBK2dRjqR",
+      "zduJceGhVmwR6wf2xUhJp9juU6rabynbi8mFwmBcKEaP8nc1mXyrDyRMmy",
     // ircPeerDiscovery
     z3o47admQjBj2QUrwHhvTB3nV1cBwQ5ZaKF7P5WELXeRyzzvBZr33QMJWTLs:
       "zduLx8mxrSWBPpTby1n9GtvxmCu6DwK9bsu26AgxesJmeEcWrVAF4NX7QK",
     // s5
     z3o47hB2zLuvBMFaWa55SmbFQe3u97dWUG4fssXXTEcT4ZvnPkPaRF14b1EF:
-      "zduPehBQc2edPXDBsxo9CM6pTnS6bpDx1MBS92GH5wLAqMnErsVKhnobD1",
+      "zduTZ7NBNtGxQxbAaDNcp7d1xta7y2MXKnjvVA3m29ohwBeeLHL1GieTPd",
   }),
 );
 const REGISTRY_ITEM_ID = "registry";
